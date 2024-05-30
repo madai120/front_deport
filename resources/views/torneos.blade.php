45ac193789a2b450a72f2aa1ba67e92d763aaec5
mada <madaictun@gmail.com>
2024-05-30 03:13:51 -0600

    design-eventos

--- resources/views/torneos.blade.php
+++ resources/views/torneos.blade.php
@@ -40,51 +40,55 @@
         </nav>
     </header>
 
-    <!-- Aquí comienza el código del carrusel -->
-    <div class="grid grid-cols-12 gap-12">
-        <div class="centered-titulo">
-            <b>TORNEOS</b>
+    <div class="grid grid-cols-12 gap-12 mt-10">
+        <div class="col-span-12 flex justify-center items-center">
+            <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-center text-white">TORNEOS</h1>
         </div>
     </div>
 
-    <!-- Implementa el carrusel aquí -->
-    <div class="relative mx-auto w-full max-w-3xl h-64 md:h-80 lg:h-96 overflow-hidden mt-32 mr-20">
-        <div class="slide relative w-full h-full hidden">
-            <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneobasquet.jpeg') }}">
-            <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de
-                    Basquetbol</b></div>
-        </div>
-
-        <div class="slide relative w-full h-full hidden">
-            <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneofut.jpg') }}">
-            <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de Futbol</b>
+    <div class="flex flex-col md:flex-row justify-center items-start gap-12 mt-12 mx-auto max-w-7xl">
+        <!-- Implementa el carrusel aquí -->
+        <div class="relative w-full max-w-3xl h-64 md:h-80 lg:h-96 overflow-hidden">
+            <div class="slide relative w-full h-full hidden">
+                <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneobasquet.jpeg') }}">
+                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de Basquetbol</b></div>
             </div>
-        </div>
 
-        <div class="slide relative w-full h-full hidden">
-            <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneovoleibol.jpeg') }}">
-            <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de Voleibol</b>
+            <div class="slide relative w-full h-full hidden">
+                <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneofut.jpg') }}">
+                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de Futbol</b></div>
             </div>
+
+            <div class="slide relative w-full h-full hidden">
+                <img class="w-full h-full object-cover" src="{{ asset('images/eventos/torneovoleibol.jpeg') }}">
+                <div class="absolute bottom-0 w-full px-5 py-3 bg-black/40 text-center text-white"><b>Torneo de Voleibol</b></div>
+            </div>
+
+            <!-- Botón anterior -->
+            <a class="absolute left-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
+                onclick="moveSlide(-1)">❮</a>
+
+            <!-- Botón siguiente -->
+            <a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
+                onclick="moveSlide(1)">❯</a>
         </div>
 
-        <!-- Botón anterior -->
-        <a class="absolute left-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(-1)">❮</a>
-
-        <!-- Botón siguiente -->
-        <a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(1)">❯</a>
+        <!-- Descripción de los torneos -->
+        <div class="description max-w-md p-5 bg-white shadow-lg rounded-md mt-8">
+            <h2 class="text-2xl font-bold mb-4">Participa en Nuestros Torneos</h2>
+            <p class="text-lg text-gray-700">¡No te pierdas la oportunidad de mostrar tus habilidades y competir en nuestros emocionantes torneos! Desde el baloncesto hasta el voleibol, tenemos una variedad de eventos diseñados para todos los niveles.</p>
+            <p class="mt-4 text-lg text-gray-700">Inscríbete ahora y vive la emoción de la competencia, conoce nuevos amigos y disfruta de un ambiente lleno de energía y deporte.</p>
+        </div>
     </div>
-    <br>
 
+    <div class="hidden md:flex justify-center items-center space-x-5 mr-96 mt-5">
     <!-- Los puntos de navegación -->
-    <div class="flex justify-center items-center space-x-5  ml-96">
-        <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(1)"></div>
-        <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(2)"></div>
-        <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(3)"></div>
-    </div>
-    <!-- Fin del carrusel -->
+    <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(1)"></div>
+    <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(2)"></div>
+    <div class="dot w-4 h-4 rounded-full cursor-pointer dot-inactive" onclick="currentSlide(3)"></div>
+</div>
 
+    
     <script src="js/welcome.js"></script>
     <script src="js/eventos.js"></script>
 </body>
