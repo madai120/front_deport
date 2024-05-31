e33715d6b5f5109541f52d1bffe8b20e42330e85
mada <madaictun@gmail.com>
2024-05-30 20:58:57 -0600

    update

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
         rel="stylesheet">
+        
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 
 </head>
